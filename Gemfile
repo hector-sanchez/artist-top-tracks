c587800646e7e195f55b85328cb46a566216de18
single-stop-dev <single-stop-dev>
2018-04-09 01:43:17 -0400

    Adds Rack CORS for local (localhost) access

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'twilio-ruby'
 gem 'rspotify', '~> 2.0'
+gem 'rack-cors', :require => 'rack/cors'
--- app/controllers/artist_top_track_requests_controller.rb
+++ app/controllers/artist_top_track_requests_controller.rb
@@ -2,13 +2,10 @@ class ArtistTopTrackRequestsController < ApplicationController
   include TopTrackMessageable
 
   def create
-    @artist_top_track_request = ArtistTopTrackRequest.create!(artist_top_track_request_params)
+    @artist_top_track_request = ArtistTopTrackRequest.create!(
+        artist_name: params["artist_name"],
+        cell_phone_number: params["cell_phone_number"]
+      )
     json_response @artist_top_track_request, :created
   end
-
-  private
-
-    def artist_top_track_request_params
-      params.permit(:artist_name, :cell_phone_number)
-    end
 end
